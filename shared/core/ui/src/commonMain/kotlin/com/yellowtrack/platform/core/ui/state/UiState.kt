a246697f7014023d7bce1b9ff6311e98cd8855d2
Clement Ngati <clement@Clements-MacBook-Pro.local>
2026-07-25 13:15:17 -0700

    core ui

---
+++ shared/core/ui/src/commonMain/kotlin/com/yellowtrack/platform/core/ui/state/UiState.kt
@@ -0,0 +1,15 @@
+package com.yellowtrack.platform.core.ui.state
+
+sealed interface UiState<out T> {
+    data object Loading : UiState<Nothing>
+
+    data object Empty : UiState<Nothing>
+
+    data class Success<T>(
+        val data: T,
+    ) : UiState<T>
+
+    data class Error(
+        val message: String,
+    ) : UiState<Nothing>
+}
